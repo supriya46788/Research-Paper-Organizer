585dd56c658f5ec661487204f2cf6b3270d5829c
Raghav123-30 <raghavendrabhat2018@gmail.com>
2025-08-13 18:51:54 +0530

    Create OTP, Paper, and User models for database schema and setup connection to MongoDB database

--- backend/src/app.js
+++ backend/src/app.js
@@ -1,13 +1,17 @@
 import express from "express";
-
+import dotenv from "dotenv";
+import { connectDB } from "./utils/db.js";
 const app = express();
 const port = 3000;
+
+dotenv.config();
 
 app.get("/", (req, res) => {
   res.json({ message: "Backend application for Research Paper Organizer" });
 });
 
-const startServer = () => {
+const startServer = async () => {
+  await connectDB();
   app.listen(port, () => {
     console.log(`Listening on port ${port}`);
   });
---
+++ backend/src/models/otp.js
@@ -0,0 +1,19 @@
+import mongoose from "mongoose";
+
+const otpSchema = new mongoose.Schema(
+  {
+    userId: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "User",
+      required: true,
+    },
+    otp: { type: String, required: true },
+    expiresAt: { type: Date, required: true },
+  },
+  {
+    timestamps: true,
+  }
+);
+
+const OTP = mongoose.model("OTP", otpSchema);
+export default OTP;
