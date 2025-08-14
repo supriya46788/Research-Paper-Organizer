c87a0659b9dc6ce57fdbbb5d7a407efbc52a84e8
Raghav123-30 <raghavendrabhat2018@gmail.com>
2025-08-14 11:28:51 +0530

    Implement user authentication with registration and login endpoints; add email sending functionality using Resend service

--- backend/src/app.js
+++ backend/src/app.js
@@ -1,6 +1,9 @@
 import express from "express";
 import dotenv from "dotenv";
 import { connectDB } from "./utils/db.js";
+import authRoutes from "./routes/authRoutes.js";
+import cors from "cors";
+import cookieParser from "cookie-parser";
 const app = express();
 const port = 3000;
 
@@ -9,6 +12,11 @@ dotenv.config();
 app.get("/", (req, res) => {
   res.json({ message: "Backend application for Research Paper Organizer" });
 });
+
+app.use(cookieParser());
+app.use(cors());
+app.use(express.json());
+app.use("/api/auth", authRoutes);
 
 const startServer = async () => {
   await connectDB();
