8a2d7d81479a2e3b4cb9f0bf1fce57383e54f598
VP-TT <vishnupriya.taduka@yahoo.com>
2025-08-25 12:02:22 +0530

    made ui changes for forgot-password only

--- backend/src/config/passport-setup.js
+++ backend/src/config/passport-setup.js
@@ -1,6 +1,7 @@
 import passport from "passport";
 import { Strategy as GoogleStrategy } from "passport-google-oauth20";
 import User from "../models/user.js";
+import 'dotenv/config';
 
 passport.use(
   new GoogleStrategy(
